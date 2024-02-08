3907fa6a04d42d0dc9ed44baeeb54fa93f158cc3
suevin choi <suevin.choi@kellogg.northwestern.edu>
2024-02-08 15:52:13 +0000

    3rd update

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   get "/tacos", :controller => "tacos", :action => "index"
   get "/dice", :controller => "dice", :action => "index"
+
+  resources "companies"
+  # get "/companies", :controller => "companies", :action => "index"
 end
